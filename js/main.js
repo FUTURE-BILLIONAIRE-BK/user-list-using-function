375c116328c87349789a738b591b1fe62cceaca6
FUTURE-BILLIONAIRE-BK <bk_khasanov@mail.ru>
2022-05-24 15:42:47 +0500

    using function

---
+++ js/main.js
@@ -0,0 +1,136 @@
+const data = [
+  {
+    src: 'https://picsum.photos/200/300?random=1',
+    id: 1,
+    first_name: "Sophronia",
+    last_name: "Stoddart",
+    email: "sstoddart0@howstuffworks.com",
+    gender: "Female",
+    ip_address: "80.215.124.45",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=2',
+    id: 2,
+    first_name: "Elie",
+    last_name: "Ashenhurst",
+    email: "eashenhurst1@ameblo.jp",
+    gender: "Female",
+    ip_address: "68.37.253.68",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=3',
+    id: 3,
+    first_name: "Elianore",
+    last_name: "Eaddy",
+    email: "eeaddy2@chronoengine.com",
+    gender: "Female",
+    ip_address: "67.148.137.105",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=4',
+    id: 4,
+    first_name: "Brittne",
+    last_name: "Heditch",
+    email: "bheditch3@businessweek.com",
+    gender: "Female",
+    ip_address: "100.224.120.91",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=5',
+    id: 5,
+    first_name: "Harman",
+    last_name: "Mashal",
+    email: "hmashal4@sakura.ne.jp",
+    gender: "Male",
+    ip_address: "135.116.148.60",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=6',
+    id: 6,
+    first_name: "Flore",
+    last_name: "Zappel",
+    email: "fzappel5@tripod.com",
+    gender: "Female",
+    ip_address: "78.191.118.28",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=7',
+    id: 7,
+    first_name: "Angelina",
+    last_name: "Manneville",
+    email: "amanneville6@lycos.com",
+    gender: "Bigender",
+    ip_address: "176.253.242.10",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=8',
+    id: 8,
+    first_name: "Merilyn",
+    last_name: "Marusik",
+    email: "mmarusik7@1und1.de",
+    gender: "Female",
+    ip_address: "221.137.214.47",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=9',
+    id: 9,
+    first_name: "Jeddy",
+    last_name: "Brunn",
+    email: "jbrunn8@umn.edu",
+    gender: "Male",
+    ip_address: "9.189.41.129",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=10',
+    id: 10,
+    first_name: "Kilian",
+    last_name: "Chadderton",
+    email: "kchadderton9@wix.com",
+    gender: "Male",
+    ip_address: "177.47.251.168",
+  },
+  {
+    src: 'https://picsum.photos/200/300?random=11',
+    id: 11,
+    first_name: "Kilian",
+    last_name: "Chadderton",
+    email: "kchadderton9@wix.com",
+    gender: "Male",
+    ip_address: "177.47.251.168",
+  },
+];
+
+const ul = document.querySelector('ul')
+const wrappper = document.createDocumentFragment(' ')
+function createTag(tagName, className)
+{
+  var tagName = document.createElement(tagName)
+  tagName.className = (className)
+  return tagName
+}
+
+function user_info( text, data)
+{
+  text.textContent = data
+  return text
+}
+
+for (let i=0; i<=data.length-1; i++)
+{
+const li = createTag('li', 'user card d-inline-block col-12 col-lg-4 col-md-6 col-xl-3 px-2')
+const div = createTag('div', 'user__content card-body')
+const userImg = createTag('img', 'user__img card-img' )
+userImg.src = data[i].src
+const userName = createTag('h3', 'user__fullname h5 mt-3')
+user_info(userName, `${data[i].first_name} ${data[i].last_name}`)
+const userEmail = createTag('p', 'user__email h6 text-primary d-block')
+user_info(userEmail, data[i].email)
+const userGender = createTag('p', 'user__gender h6')
+user_info(userGender, data[i].gender)
+const userAddress = createTag('p', 'user__address text-bg-info rounded text-white ps-2')
+user_info(userAddress, data[i].ip_address)
+div.append(userImg, userName, userEmail, userGender, userAddress)
+li.append(div)
+wrappper.append(li)
+}
+ul.append(wrappper)
